2f742cad6e7d721e33bcf410a923fd066930d625
FridayMood <iamaliaksandra@gmail.com>
2023-07-05 18:26:50 +0200

    Task 11 - improvement

--- src/Lesson2/Task11.java
+++ src/Lesson2/Task11.java
@@ -2,7 +2,7 @@ package Lesson2;
 
 public class Task11 {
     public static void main(String[] args) {
-        int b = 0;
+        int b;
         for (int i = 10; i <= 20; i++) {
             b = i * i;
             System.out.println(b);
