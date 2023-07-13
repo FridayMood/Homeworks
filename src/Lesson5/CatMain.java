57ebad260490dddea46203d12fbe8873fa68bb58
FridayMood <iamaliaksandra@gmail.com>
2023-07-13 16:02:47 +0200

    Cat with scanner improvement

--- src/Lesson5/CatMain.java
+++ src/Lesson5/CatMain.java
@@ -6,7 +6,10 @@ public class CatMain {
     public static void main(String[] args) {
         Cat cat = new Cat();
         cat.foodAmount = 5.00;
-        boolean notHungry = cat.feed(4.99);
+        Scanner sc = new Scanner (System.in);
+        double i;
+        System.out.println("Введите количество корма: ");
+        boolean notHungry = cat.feed(i = sc.nextDouble());
         if (notHungry) {
             System.out.println("Кот не наелся");
         } else {
