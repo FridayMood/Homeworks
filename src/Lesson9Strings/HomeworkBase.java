837e8707244e359ce0f9b49fc257cdb22b96516a
FridayMood <iamaliaksandra@gmail.com>
2023-07-27 14:56:59 +0200

    String HW update

--- src/Lesson9Strings/HomeworkBase.java
+++ src/Lesson9Strings/HomeworkBase.java
@@ -2,13 +2,13 @@ package Lesson9Strings;
 
 public class HomeworkBase implements MethodsForHomework {
     public static void main(String[] args) {
-        System.out.println("Original Line: 3454-Tyu-6785-tyk-6u8i");
-        String docNumber = MethodsForHomework.FirstBlockOfNumbers("3454-Tyu-6785-tyk-6u8i");
-        docNumber = MethodsForHomework.replace3Letters("3454-Tyu-6785-tyk-6u8i");
-        docNumber = MethodsForHomework.onlyLetters("3454-Tyu-6785-tyk-6u8i");
-        docNumber = MethodsForHomework.startsWith("7774-Tyu-6785-tyk-6u8i");
-        docNumber = MethodsForHomework.endsWith("7774-Tyu-6785-tyk-1a2b");
-        docNumber = MethodsForHomework.containsElement("7774-Tyu-6785-ABC-1a2b");
+        String str = "8764-Tyu-6785-tyk-6u8i";
+        String docNumber = MethodsForHomework.FirstBlockOfNumbers(str);
+        docNumber = MethodsForHomework.replace3Letters(str);
+        docNumber = MethodsForHomework.onlyLetters(str);
+        docNumber = MethodsForHomework.startsWith(str);
+        docNumber = MethodsForHomework.endsWith(str);
+        docNumber = MethodsForHomework.containsElement(str);
 
 
 
